e13350c1dab948d613e78b6b6c926cb8b99b1a82
Lucy Alupo <89966514+Lucy152@users.noreply.github.com>
2021-10-04 16:55:21 +0300

    Javascript work

---
+++ ass8.js
@@ -0,0 +1,25 @@
+function name(name,age) {
+  console.log("My name is "+name+" and i am "+age+ " years old");
+}
+name("Opeto Leonard",32);
+name("Alupo Lucy",25);
+name("Outa Patrick",57);
+name("Apio Mercy",23);
+name("Etii Faith",45);
+
+function choose(Gender) {
+  var fruit =  gender("female");
+  console.log("This is a " + gender);
+}
+function gender(gender) {
+  return gender;
+}
+choose(gender);
+
+let Country =(first,second) =>  results = first + " " + second;
+console.log(country("Sierre","Leon"));
+
+for(let i=0; i < 19 ; i++){
+  console.log("i love uganda" );
+}
+
